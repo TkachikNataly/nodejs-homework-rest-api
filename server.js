1f9315f671d6c8844dee07fd2ad52487ecc8ab87
TkachikNataly <tkachik.nataly@gmail.com>
2022-07-22 02:16:45 +0300

    mongodb

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+// const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const { DB_HOST } = process.env;
+
+console.log(DB_HOST);
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Database connect success"))
+  .catch(error => error.message)
