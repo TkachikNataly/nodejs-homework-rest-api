e375134618455ff425ecce978e4cbe3134a8b8aa
TkachikNataly <tkachik.nataly@gmail.com>
2022-08-08 00:19:50 +0300

    Summary 1

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,7 +3,7 @@ const { Contact } = require("../../models/contacts");
 const getAll = async (req, res, next) => {
     try {
         const { _id: owner } = req.user;
-        const { page = 1, limit = 2 } = req.query;
+        const { page = 1, limit = 20 } = req.query;
         const skip = (page - 1) * limit;
         const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
             skip,
